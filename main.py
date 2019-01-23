1a3a2d3aa915952ee6b5fe7c04a17d62d10cc6e6
maelstrom849 <46925458+maelstrom849@users.noreply.github.com>
2019-01-23 11:21:44 +0000

    Updated main.py
    Added comments

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
 
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 from time import sleep
 import selenium
 
-
+# createitemlist method only needs to be called once and creates a list of all possible upgrades
+# for the program to refer back to.
 def createitemlist(driver):
     item0 = driver.find_element_by_id('product0')
     item1 = driver.find_element_by_id('product1')
@@ -27,9 +27,11 @@ def createitemlist(driver):
 
     return item_list
 
-
+# checkupgrade simply checks whether the first upgrade in the list is available
 def checkupgrade(driver):
     try:
+        # cookie cliker automatically sorts upgrades to show the cheapest one first, so 
+        # this will always check whether the cheapest can be bought right now
         upgrade = driver.find_element_by_id('upgrade0')
         upgrade.click()
     except selenium.common.exceptions.ElementNotInteractableException:
@@ -37,7 +39,8 @@ def checkupgrade(driver):
     except selenium.common.exceptions.StaleElementReferenceException:
         pass
 
-
+# checkitems uses the item list created earlier and iterates through it in reverse order
+# to see what items can be bought, starting with the highest level item available
 def checkitems(item_list):
     for i in range(len(item_list)):
         try:
@@ -51,26 +54,19 @@ def checkitems(item_list):
 
 
 def main():
+    # uses webdriver_manager to automatically install Firefox webdriver
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     driver.get('http://orteil.dashnet.org/cookieclicker/')
+    # checks whether 'Cookie' is in the title of the website
     assert 'Cookie' in driver.title
-
+    # waits for 2 seconds to continue script, in order to give the website a chance to set itself up
     sleep(2)
-
+    #set important variables for the general game
     bigcookie = driver.find_element_by_id("bigCookie")
     item_list = createitemlist(driver)
-    checkitems(item_list)
-
+    # every 15 clicks, the program will check if upgrades or units can be bought
     checkiteminterval = 15
-    try:
-        for i in range(checkiteminterval):
-            bigcookie.click()
-        checkitems(item_list)
-    except selenium.common.exceptions.NoSuchWindowException:
-        print("Oh no, you've closed the window or something")
-
-    checkupgrade(driver)
-
+    # this loop just continues going until the user closes the window, at which point it prints the print statement
     while True:
         try:
             for i in range(checkiteminterval):
